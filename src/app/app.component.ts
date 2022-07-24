930c0a0acd37061375a151317908e7ef25012571
Gabriel Martins Carossi <gabrielmcarossi@gmail.com>
2022-07-24 20:17:44 -0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .whiteText {
+        color:white;
+    }
+  `]
 })
 export class AppComponent {
+  displayDetails = false;
+  logs = [];
+  
+  onToggleDisplayDetails(){
+    this.displayDetails = !this.displayDetails;
+    this.logs.push(this.logs.length + 1);
+  }
 }
