9327340037c1283d1dba6897dadcb7b8bb26759b
Gabriel Martins Carossi <gabrielmcarossi@gmail.com>
2022-07-24 16:09:34 -0300

    34_two_way_binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false; 
-  serverCreationStatus = 'No server was created.'
+  serverCreationStatus = 'No server was created.';
+  serverName = 'defaultServer';
 
   constructor() {
     setTimeout(() => {
@@ -20,6 +21,10 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: any){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
