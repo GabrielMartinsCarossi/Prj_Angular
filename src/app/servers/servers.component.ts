5b039cb64a43a8f6446c28abd8ec2525ab3aac73
Gabriel Martins Carossi <gabrielmcarossi@gmail.com>
2022-07-24 16:44:14 -0300

    Data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -20,7 +20,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus = 'Server was created with name: ' + this.serverName;
   }
 
   onUpdateServerName(event: any){
