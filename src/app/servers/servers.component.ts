6b49d61d352a2658d4ef44789d710c33fb4a72ea
Gabriel Martins Carossi <gabrielmcarossi@gmail.com>
2022-07-24 18:06:10 -0300

    40_ngIf

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false; 
   serverCreationStatus = 'No server was created.';
-  serverName = 'defaultServer';
+  serverName = '';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -21,6 +22,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created with name: ' + this.serverName;
+    this.serverCreated = true;
   }
 
   onUpdateServerName(event: any){
