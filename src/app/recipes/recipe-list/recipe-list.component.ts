d490cb8cbf7be7325cb5692238d221d314bb5bbc
Gabriel Martins Carossi <gabrielmcarossi@gmail.com>
2022-07-24 22:39:57 -0300

    '55_adding_list_of_recipes'

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Chocolate Chips Cookies', 'Super simple cookies recipe', 'https://www.errenskitchen.com/wp-content/uploads/2019/02/chocolate-chip-cookies-3.jpg'),
+    new Recipe('Amazing Carbonara', 'The original Italian recipe', 'https://assets.bonappetit.com/photos/5e46c93bc7bda40009794ace/1:1/w_2560%2Cc_limit/0220-Traditional-Carbonara-Pasta.jpg')
+  ];
 
   constructor() { }
 
